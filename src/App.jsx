f1429951fed8e7754d52267925e0dde2d6536f0e
Tubagus Aldi <tubagusaldi28@gmail.com>
2024-10-14 14:02:10 +0700

    menambahkan img Slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import "./App.css";
 import Card from "./components/Card";
+import { useEffect, useState } from "react";
 
 function App() {
+  // slider for img
+  const intro_img = [
+    ["SlideShow/ff.webp", "Free Fire"],
+    ["SlideShow/ml.jpg", "Mobile Legends"],
+  ];
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % intro_img.length);
+    }, 2000); // Ganti gambar setiap 3 detik
+
+    return () => clearInterval(interval); // Bersihkan interval saat komponen unmount
+  }, [intro_img.length]);
+
   return (
     <main>
+      <div className="container-slide">
+        <img
+          className="mySlides"
+          src={intro_img[currentSlide][0]}
+          alt={intro_img[currentSlide][1]}
+        />
+      </div>
       <Card />
     </main>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,32 +4,32 @@ function Card() {
       <h2>Game Populer</h2>
       <div className="grid-container">
         <div className="card">
-          <img src="ml.jpeg" alt="logo mobile legends" />
+          <img src="gameLogo/ml.jpeg" alt="logo mobile legends" />
           <h3>Mobile Legends</h3>
         </div>
 
         <div className="card">
-          <img src="ff.jpeg" alt="" />
+          <img src="gameLogo/ff.jpeg" alt="" />
           <h3>Free Fire</h3>
         </div>
 
         <div className="card">
-          <img src="pubg.jpeg" alt="" />
+          <img src="gameLogo/pubg.jpeg" alt="" />
           <h3>PUBG</h3>
         </div>
 
         <div className="card">
-          <img src="Hok.jpeg" alt="" />
-          <h3>honor Of king</h3>
+          <img src="gameLogo/Hok.jpeg" alt="" />
+          <h3>Honor Of king</h3>
         </div>
 
         {/* <div className="card">
-          <img src="aov.jpeg" alt="" />
+          <img src="gameLogo/aov.jpeg" alt="" />
           <h3>Arena of valor</h3>
         </div>
 
         <div className="card">
-          <img src="honkai.jpeg" alt="" />
+          <img src="gameLogo/honkai.jpeg" alt="" />
           <h3>Honkai Impack</h3>
         </div> */}
       </div>
