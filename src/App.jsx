d013f40633eda516d764676fb5f759dcc8f75435
Tubagus Aldi <tubagusaldi28@gmail.com>
2024-10-14 14:20:49 +0700

    rapiin

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
 import "./App.css";
 import Card from "./components/Card";
-import { useEffect, useState } from "react";
+import ImgShows from "./components/imgshow";
 
-function App() {
-  // slider for img
-  const intro_img = [
-    ["SlideShow/ff.webp", "Free Fire"],
-    ["SlideShow/ml.jpg", "Mobile Legends"],
-  ];
-
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prev) => (prev + 1) % intro_img.length);
-    }, 2000); // Ganti gambar setiap 3 detik
-
-    return () => clearInterval(interval); // Bersihkan interval saat komponen unmount
-  }, [intro_img.length]);
-
+export default function App() {
   return (
     <main>
-      <div className="container-slide">
-        <img
-          className="mySlides"
-          src={intro_img[currentSlide][0]}
-          alt={intro_img[currentSlide][1]}
-        />
-      </div>
+      <ImgShows />
       <Card />
     </main>
   );
 }
-
-export default App;
