d6ce54fd8c3e06f5a81b84d6b8fb6e8a0e816b0d
asriqul <didipbnc11@gmail.com>
2024-10-23 21:16:43 +0700

    fix responsive sm, md, lg

--- src/components/Fragments/Cards.jsx
+++ src/components/Fragments/Cards.jsx
@@ -4,7 +4,7 @@ const Cards = (props) => {
   const { title, img, alt } = props;
   return (
     <div className='mx-auto px-6 flex flex-wrap justify-center gap-2 sm:flex sm:flex-wrap sm:gap-2 sm:justify-center'>
-      <div className='rounded-2xl shadow-md w-24 sm:w-32 md:w-64'>
+      <div className='rounded-2xl shadow-md w-24 sm:w-32 md:w-64 mb-5'>
         <img src={img} alt={alt} className='rounded-2xl' />
         <div className='px-3 py-2'>
           <Label children={title} variant="font-bold text-[1rem] text-center sm:text-md sm:text-center md-text-2xl md:text-center" />
